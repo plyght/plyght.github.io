046af67390f88532e489aa3ee3891dd7ab24ba5f
plyght <nico.c.jaffer@gmail.com>
2023-12-18 14:31:03 -0500

    wow

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// Function to display a greeting message in the console
+function greet() {
+    console.log("Hello, welcome to my website!");
+}
+
+// Call the greet function when the page loads
+window.onload = greet;
